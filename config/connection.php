a0c40573fe24f5b142b2466b99884fea60a29cfd
Nytri <llTheBlankll@gmail.com>
2023-04-01 17:58:32 +0800

    Connection Configuration Changed

--- config/connection.php
+++ config/connection.php
@@ -1,8 +1,13 @@
 <?php
-define("MYSQL_USERNAME", "u937809650_root");
-define("MYSQL_PASSWORD", "Group_07");
+# define("MYSQL_USERNAME", "u937809650_root");
+# define("MYSQL_PASSWORD", "Group_07");
+# define("MYSQL_HOST", "localhost");
+# define("MYSQL_DATABASE", "u937809650_test");
+
+define("MYSQL_USERNAME", "public");
+define("MYSQL_PASSWORD", "public");
 define("MYSQL_HOST", "localhost");
-define("MYSQL_DATABASE", "u937809650_test");
+define("MYSQL_DATABASE", "test");
 
 function getConnection(): mysqli
 {
